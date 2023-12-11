2a5b3d1a1c41b40691758279223a1443cb8f6ed2
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2023-12-11 16:56:53 +0800

    rough script for generating mock report data

--- createMock.sql
+++ createMock.sql
@@ -18,23 +18,88 @@ CREATE TABLE reports (
 
 INSERT INTO reports (report_uid, version_number, date, shift, end_ring, end_chainage, reported_by, status) 
 VALUES 
-    (uuid_generate_v4(), 64, '2023-08-04 06:41:02', 'sed', 82, 563.55, 'Pink', 'arcu'), 
-    (uuid_generate_v4(), 72, '2023-11-09 07:26:00', 'curae', 100, 585.68, 'Puce', 'proin'),
-    (uuid_generate_v4(), 95, '2023-02-23 05:18:43', 'pharetra', 75, 322.17, 'Turquoise', 'vestibulum'), 
-    (uuid_generate_v4(), 36, '2023-07-07 06:08:06', 'pede', 57, 548.9, 'Fuscia', 'integer'), 
-    (uuid_generate_v4(), 17, '2023-02-02 09:03:37', 'vulputate', 74, 969.91, 'Blue', 'orci');
+    (uuid_generate_v4(), 1, '2023-08-04 10:41:02', 'DS', 82, 19596.54, 'mohammad.firdaus@glcwtp.com.au', 'draft'), 
+    (uuid_generate_v4(), 1, '2023-11-09 04:26:00', 'NS', 100, 19658.54, 'anurag.pattekar@glcwtp.com.au', 'rejected'),
+    (uuid_generate_v4(), 1, '2023-02-23 13:18:43', 'DS', 75, 19600.34, 'alvin.francis@glcwtp.com.au', 'approved'), 
+    (uuid_generate_v4(), 1, '2023-07-07 08:08:06', 'DS', 57, 19172.27, 'anurag.pattekar@glcwtp.com.au', 'submitted'), 
+    (uuid_generate_v4(), 1, '2023-02-02 21:03:37', 'NS', 74, 19190.92, 'mohammad.firdaus@glcwtp.com.au', 'draft');
 
-INSERT INTO reports_versions (version_number, data, updated_at) 
+UPDATE reports 
+SET 
+    version_number = null;
+    date = null;
+
+
+
+INSERT INTO reports_versions (report_uid, version_number, data, updated_at) 
 VALUES 
-    (64, '[{},{}]', '2023-07-25 07:34:02'), 
-    (72, '[{},{}]', '2023-04-16 21:43:34'), 
-    (95, '[{},{}]', '2022-12-02 17:59:05'), 
-    (36, '[{},{}]', '2022-12-23 23:38:52'), 
-    (17, '[{},{}]', '2023-04-04 02:49:51');
+    ('d1f4bf3b-6d2d-475c-b519-f0fce27377ca',
+    1, 
+    '{
+        "formRef": "OPEB-DS-20231108070000",
+        "savedDowntime": [
+            {
+                "endTimestamp": "2023-11-08T04:54:25+00:00",
+                "startTimestamp": "2023-11-07T19:30:00+00:00",
+                "status": "STOPPED"
+            }
+        ]
+    }'::JSONB, 
+    '2023-07-25 07:34:02'), 
+    ('f8045c84-2dcf-4259-8e9f-2b57b1bb8512',
+    1, 
+    '{
+        "formRef": "OPEB-DS-20231012070000",
+        "savedDowntime": [
+            {
+                "endTimestamp": "2023-10-12T04:54:25+00:00",
+                "startTimestamp": "2023-10-11T19:30:00+00:00",
+                "status": "ADVANCE"
+            }
+        ]
+    }'::JSONB, 
+    '2023-04-16 21:43:34'), 
+    ('4c5b3c5e-e094-4d94-bb78-fcedd1a9d94d',
+    1, 
+    '{
+        "formRef": "OPEB-NS-20231207190000",
+        "savedDowntime": [
+            {
+                "endTimestamp": "2023-12-07T07:54:35+00:00",
+                "startTimestamp": "2023-12-07T07:49:10+00:00",
+                "status": "ADVANCE"
+            }
+        ]
+    }'::JSONB, 
+    '2022-12-02 17:59:05'), 
+    ('277002ba-0b01-47fe-8ac4-b1cf37e47c7f',
+    1, 
+    '{
+        "formRef": "OPEB-DS-20230929070000",
+        "savedDowntime": [
+            {
+                "endTimestamp": "2023-09-29T08:59:56+00:00",
+                "startTimestamp": "2023-09-28T21:00:01+00:00",
+                "status": "RINGBUILD"
+            }
+        ]
+    }'::JSONB, 
+    '2022-12-23 23:38:52'), 
+    ('a8476032-2f08-46bd-96a5-c6e55506ef25',
+    1,    
+    '{
+        "formRef": "OPWB-NS-20231024190000",
+        "savedDowntime": [
+            {
+                "endTimestamp": "2023-10-24T11:16:12+00:00",
+                "startTimestamp": "2023-10-24T11:08:00+00:00",
+                "status": "REGRIP"
+            }
+        ]
+    }'::JSONB, 
+    '2023-04-04 02:49:51');
 
-ALTER TABLE reports_versions DROP CONSTRAINT IF EXISTS report_uid;
 ALTER TABLE reports_versions ADD CONSTRAINT report_uid FOREIGN KEY (report_uid) REFERENCES reports (report_uid);
-UPDATE reports_versions as B SET report_uid = A.report_uid FROM reports as A WHERE B.version_number = A.version_number;
 
 CREATE TABLE downtime (downtime_id smallserial PRIMARY KEY NOT NULL, name text NOT NULL);
 
@@ -89,3 +154,26 @@ ALTER TABLE cycle_time ADD CONSTRAINT report_uid FOREIGN KEY (report_uid) REFERE
 ALTER TABLE cycle_time ADD CONSTRAINT downtime_id FOREIGN KEY (downtime_id) REFERENCES downtime (downtime_id);
 ALTER TABLE cycle_time ADD CONSTRAINT breakdown_id FOREIGN KEY (breakdown_id) REFERENCES breakdown (breakdown_id);
 ALTER TABLE cycle_time ADD CONSTRAINT status_id FOREIGN KEY (status_id) REFERENCES status (status_id);
+
+ALTER TABLE reports_versions
+ALTER COLUMN data TYPE jsonb USING data::jsonb;
+
+CREATE TABLE downtime_breakdown_rl (
+    downtime_id smallint NOT NULL,
+    breakdown_id smallint NOT NULL
+);
+
+ALTER TABLE downtime_breakdown_rl ADD CONSTRAINT downtime_id FOREIGN KEY (downtime_id) REFERENCES downtime;
+ALTER TABLE downtime_breakdown_rl ADD CONSTRAINT breakdown_id FOREIGN KEY (breakdown_id) REFERENCES breakdown;
+
+INSERT INTO downtime_breakdown_rl (downtime_id, breakdown_id) 
+VALUES 
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (2, 2),
+    (2, 3);
+
+
+
+
---
+++ report-generator.py
@@ -0,0 +1,48 @@
+import random
+import string
+
+# Function to generate mock data for different data types
+def generate_mock_data(data_type):
+    if data_type == 'integer':
+        return str(random.randint(1, 100))
+    elif data_type == 'text':
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(5, 15)))
+    elif data_type == 'date':
+        return f"'{random.randint(2000, 2022)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}'"
+    # Add more data types as needed
+
+# Define table schema and columns
+table_name = 'example_table'
+columns = {
+    'id': 'SERIAL PRIMARY KEY',
+    'name': 'TEXT',
+    'age': 'INTEGER',
+    'birthdate': 'DATE'
+    # Add more columns with their data types
+}
+
+# Generate SQL query to create table
+create_table_query = f'CREATE TABLE {table_name} ('
+for column, data_type in columns.items():
+    create_table_query += f'{column} {data_type}, '
+create_table_query = create_table_query[:-2] + ');'
+
+print("SQL Query to Create Table:")
+print(create_table_query)
+
+# Generate SQL queries to insert mock data
+insert_queries = []
+num_records_to_insert = 10
+for _ in range(num_records_to_insert):
+    insert_query = f"INSERT INTO {table_name} ("
+    for column in columns.keys():
+        insert_query += f"{column}, "
+    insert_query = insert_query[:-2] + ") VALUES ("
+    for data_type in columns.values():
+        insert_query += f"{generate_mock_data(data_type)}, "
+    insert_query = insert_query[:-2] + ");"
+    insert_queries.append(insert_query)
+
+print("\nSQL Queries to Insert Mock Data:")
+for query in insert_queries:
+    print(query)
